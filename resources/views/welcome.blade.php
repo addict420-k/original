ad61abcf1e32344e62b169174b02d0724eeefef6
addict420 <yakubutsu-izonsho@addict420.com>
2019-12-12 08:43:40 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <div class="text-center">
             <h1>煙切りへようこそ</h1>
             <h4>煙との縁を切りましょう</h4>
+            {!! link_to_route('signup.get', '新規登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
